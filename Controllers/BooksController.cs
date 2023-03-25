954928811f2ebfb1b72f03c0a68b17282ede7f46
ashishgangwar07 <ashishgangwar07@gmail.com>
2023-03-25 09:39:54 +0530

    Added AddNewBook Action  method

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using BookStore.API.Repository;
+﻿using BookStore.API.Models;
+using BookStore.API.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -30,7 +31,13 @@ namespace BookStore.API.Controllers
                 return NotFound();
             }
             return Ok(book);
+        }
 
+        [HttpPost("")]
+        public async Task<IActionResult> AddNewBook([FromBody]BookModel bookModel)
+        {
+            var id = await _bookRepository.AddBookAsync(bookModel);
+            return CreatedAtAction(nameof(GetBookById), new { id = id, controller = "books" }, id);
         }
     }
 }
