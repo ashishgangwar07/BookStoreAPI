b15338798956c75ccfae0086ed9d994b5754d0bc
ashishgangwar07 <ashishgangwar07@gmail.com>
2023-03-25 09:13:23 +0530

    Added GetBookById method

--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -24,5 +24,16 @@ namespace BookStore.API.Repository
             }).ToListAsync();
             return records;
         }
+
+        public async Task<BookModel> GetBookByIdAsync(int bookId)
+        {
+            var record= await _context.Books.Where(x => x.Id==bookId).Select(x => new BookModel()
+            {
+                Id = x.Id,
+                Title= x.Title,
+                Description = x.Description,
+            }).FirstOrDefaultAsync();
+            return record;
+        }
     }
 }
